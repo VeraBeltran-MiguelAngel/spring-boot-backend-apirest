a1ed9dd013c7a40d3371d0c431af7251226e68aa
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-09-27 11:34:56 -0600

    Cambios video 332
    Añadiendo Datos de pueba
    Al ejecutar el app en localhost:8080/api/clientes
    Obesrvamos la lista de clientes en formato json

---
+++ src/main/resources/import.sql
@@ -0,0 +1,15 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('John', 'Doe', 'john.doe@gmail.com', '2017-08-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2017-08-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jane', 'Doe', 'jane.doe@gmail.com', '2017-08-04');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', '2017-08-05');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Erich', 'Gamma', 'erich.gamma@gmail.com', '2017-08-06');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Richard', 'Helm', 'richard.helm@gmail.com', '2017-08-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ralph', 'Johnson', 'ralph.johnson@gmail.com', '2017-08-08');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('John', 'Vlissides', 'john.vlissides@gmail.com', '2017-08-09');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('James', 'Gosling', 'james.gosling@gmail.com', '2017-08-010');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Bruce', 'Lee', 'bruce.lee@gmail.com', '2017-08-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Johnny', 'Doe', 'johnny.doe@gmail.com', '2017-08-12');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('John', 'Roe', 'john.roe@gmail.com', '2017-08-13');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jane', 'Roe', 'jane.roe@gmail.com', '2017-08-14');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Richard', 'Doe', 'richard.doe@gmail.com', '2017-08-15');
